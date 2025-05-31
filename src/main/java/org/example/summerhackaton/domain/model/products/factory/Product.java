209cdb626dffbe330e68ff4423eda29f0aee3c2e
nachetto <itsnachetto@gmail.com>
2025-05-31 20:38:21 +0200

    fix compliation

--- src/main/java/org/example/summerhackaton/domain/model/products/factory/Product.java
+++ src/main/java/org/example/summerhackaton/domain/model/products/factory/Product.java
@@ -1,17 +1,12 @@
 package org.example.summerhackaton.domain.model.products.factory;
 
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.math.BigDecimal;
+import java.util.Objects;
 
 @Document(collection = "products")
-@Data
-@Builder
-@NoArgsConstructor
 public abstract class Product {
     @Id
     private String id;
@@ -20,4 +15,85 @@ public abstract class Product {
     private BigDecimal price;
     private int quantity;
 
+    public Product() {
+    }
+
+    public Product(String id, String name, ProductType type, BigDecimal price, int quantity) {
+        this.id = id;
+        this.name = name;
+        this.type = type;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    // Getters y setters
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public ProductType getType() {
+        return type;
+    }
+
+    public void setType(ProductType type) {
+        this.type = type;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    // equals y hashCode
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return quantity == product.quantity &&
+                Objects.equals(id, product.id) &&
+                Objects.equals(name, product.name) &&
+                type == product.type &&
+                Objects.equals(price, product.price);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, type, price, quantity);
+    }
+
+    // toString
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", type=" + type +
+                ", price=" + price +
+                ", quantity=" + quantity +
+                '}';
+    }
 }
