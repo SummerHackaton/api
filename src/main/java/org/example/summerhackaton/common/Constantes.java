ae0e801d52addc4fb24d45e8b8aceb9489f0d0a0
Marcos <marcos.sanchez149@educa.madrid.org>
2025-05-31 09:28:41 +0200

    Cambios mínimos

--- src/main/java/org/example/summerhackaton/common/Constantes.java
+++ src/main/java/org/example/summerhackaton/common/Constantes.java
@@ -5,7 +5,7 @@ public class Constantes {
     public static final String PATH_LOGIN = "/login";
     public static final String PATH_PATIENTS = "/patients";
     public static final String PATH_ACTIVATE = "/activate/";
-    public static final String PATH_MED_RECORD = "/medRecord";;
+    public static final String PATH_MED_RECORD = "/medRecord";
     public static final String PATH_DOCTOR = "/doctor";
     public static final String JWT = "JWT";
     public static final String DETAIL = "/detail";
--- src/main/java/org/example/summerhackaton/domain/model/products/factory/food/FoodProduct.java
+++ src/main/java/org/example/summerhackaton/domain/model/products/factory/food/FoodProduct.java
@@ -4,10 +4,12 @@ import lombok.Getter;
 import lombok.Setter;
 import org.example.summerhackaton.domain.model.products.factory.Product;
 
+import java.time.LocalDate;
+
 @Getter
 @Setter
 public class FoodProduct extends Product {
-    private String expirationDate;
+    private LocalDate expirationDate;
     private boolean isPerishable;
 
     public FoodProduct () {
