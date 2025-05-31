8151cad433b3759025ce09df7ad18b0569019d14
nachetto <itsnachetto@gmail.com>
2025-05-31 22:57:50 +0200

    fix login endpoint

--- src/main/java/org/example/summerhackaton/ui/LoginController.java
+++ src/main/java/org/example/summerhackaton/ui/LoginController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Set;
 
 @RestController
+@RequestMapping("/api/1.0")
 public class LoginController {
     private final ServicioLogin servicioLogin;
 
