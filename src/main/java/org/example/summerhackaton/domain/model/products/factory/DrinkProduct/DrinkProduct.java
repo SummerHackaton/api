bdc4739687c606aefb6f6921f6382a396943c050
nachetto <itsnachetto@gmail.com>
2025-05-31 22:06:30 +0200

    common fixes

--- src/main/java/org/example/summerhackaton/domain/model/products/factory/DrinkProduct/DrinkProduct.java
+++ src/main/java/org/example/summerhackaton/domain/model/products/factory/DrinkProduct/DrinkProduct.java
@@ -1,7 +1,9 @@
 package org.example.summerhackaton.domain.model.products.factory.DrinkProduct;
 
 import org.example.summerhackaton.domain.model.products.factory.Product;
+import org.springframework.data.mongodb.core.mapping.Document;
 
+@Document(collection = "products")
 public class DrinkProduct extends Product {
 
 }
--- src/main/java/org/example/summerhackaton/domain/model/products/factory/clothes/ClothingProductFactory.java
+++ src/main/java/org/example/summerhackaton/domain/model/products/factory/clothes/ClothingProductFactory.java
@@ -3,11 +3,13 @@ package org.example.summerhackaton.domain.model.products.factory.clothes;
 import org.example.summerhackaton.domain.model.products.factory.Product;
 import org.example.summerhackaton.domain.model.products.factory.ProductFactory;
 import org.example.summerhackaton.domain.model.products.factory.ProductType;
+import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.stereotype.Component;
 
 import java.util.Map;
 
 @Component
+@Document(collection = "products")
 public class ClothingProductFactory implements ProductFactory {
     @Override
     public Product createProduct(Map<String, Object> attributes) {
